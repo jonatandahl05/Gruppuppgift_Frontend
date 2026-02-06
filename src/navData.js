155bf938ade05fb4321f2a824b11b1db41e5c74c
kostas900402 <nascoskostas@gmail.com>
2026-02-06 08:15:09 +0100

    för navigering med undermenyer la jag till navData.js med en metod export menuData, den kan uppdateras med mer eller annat, jag hade bara inga bättre ideer när jag skrev det

---
+++ src/navData.js
@@ -0,0 +1,34 @@
+// navData.js
+export const menuData = [
+    {
+        key: "people",
+        label: "Characters",
+        subItems: [
+            { label: "All", action: "list", resource: "people" },
+            { label: "Favourites", action: "favorites", resource: "people" },
+            { label: "Dark Side", action: "filter", resource: "people", filter: "dark" },
+            { label: "Light Side", action: "filter", resource: "people", filter: "light" }
+        ]
+    },
+    {
+        key: "planets",
+        label: "Planets",
+        subItems: [
+            { label: "All", action: "list", resource: "planets" }
+        ]
+    },
+    {
+        key: "starships",
+        label: "Starships",
+        subItems: [
+            { label: "All", action: "list", resource: "starships" }
+        ]
+    },
+    {
+        key: "films",
+        label: "Movies",
+        subItems: [
+            { label: "All", action: "list", resource: "films" }
+        ]
+    }
+];
