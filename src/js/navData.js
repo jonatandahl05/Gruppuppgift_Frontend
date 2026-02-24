7c228d6c9b893064fb27a37beef4ef3b4ecfe859
rasha knifdi <rasha.knifdi@chasacademy.se>
2026-02-24 08:43:05 +0100

    ny Förbättra Navigering task#34

--- src/js/navData.js
+++ src/js/navData.js
@@ -3,10 +3,8 @@ export const menuData = [
     { key: "people", label: "Characters", action: "list", resource: "people" },
     { key: "light", label: "Light Side", action: "filter", resource: "people", filter: "light" },
     { key: "dark", label: "Dark Side", action: "filter", resource: "people", filter: "dark" },
-
     { key: "planets", label: "Planets", action: "list", resource: "planets" },
     { key: "starships", label: "Starships", action: "list", resource: "starships" },
-
     { key: "films", label: "Movies", action: "list", resource: "films" },
     { key: "skywalker", label: "Skywalker Saga", action: "filter", resource: "films", filter: "skywalker" },
     { key: "vehicles", label: "Vehicles", action: "list", resource: "vehicles" },
