f0dabb13aa35f3f6610379a3ba1dc69736758799
kostas900402 <nascoskostas@gmail.com>
2026-02-25 15:13:07 +0100

    behåller style.css istället. orkar inte göra om detta en gång till

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,4 @@
-import "../css/base.css";
-import "../css/layout.css";
-import "../css/responsive.css";
-import "../css/components.css";
-import "../css/sort.css";
+import "../css/style.css";
 
 import { renderNav, initNav } from "./nav.js";
 import { initOfflineBanner } from "./offlineBanner.js";
