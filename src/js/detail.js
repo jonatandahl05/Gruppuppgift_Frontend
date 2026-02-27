033ba0d7374006ee500f2599bf897a73b047e6e5
Jonathan Isaksson <tignaltus@gmail.com>
2026-02-27 04:47:30 +0100

    Ersatte gammal bilder hämtning
    Tog bort Placeholder const och getImage då vi importerar dessa via media.js istället. Sparar mycket utrymme och lättare att läsa

--- src/js/detail.js
+++ src/js/detail.js
@@ -4,18 +4,7 @@
 import { toggleFavorite, isFavorite, normalizeType } from "./favStore.js";
 import { aboutSections } from "./aboutSections.js";
 import { fetchById } from "./fetchData.js";
-
-const PLACEHOLDER_IMG = "placeholder/198-1986030_pixalry-star-wars-icons-star-wars-ilustraciones.png";
-
-function getImage(type, id) {
-    const base = "https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img";
-    return {
-        people: `${base}/characters/${id}.jpg`,
-        planets: `${base}/planets/${id}.jpg`,
-        starships: `${base}/starships/${id}.jpg`,
-        films: `${base}/films/${id}.jpg`
-    }[type];
-}
+import { getImage, PLACEHOLDER_IMG } from "./media.js";
 
 export async function renderDetail(type, id) {
     type = normalizeType(type);
